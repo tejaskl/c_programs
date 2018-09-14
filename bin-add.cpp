b2c29b7bc3a32ab5f8ec856fb0a8dae13cec8201
tejaskl <43215767+tejaskl@users.noreply.github.com>
2018-09-14 19:31:43 +0530

    Add files via upload

---
+++ bin-add.cpp
@@ -0,0 +1,64 @@
+#include<stdio.h>
+main()
+/* code by Tejas KL
+		https://github.com/tejaskl/
+		
+   and Vijay Krishnan
+   		https://github.com/vvijayk1999/
+		   
+ */
+{
+	int a[5]={0},b[5]={0},c[5]={0},i,k=0;
+	printf("Enter the binary number with spaces       :");
+	for(i=1;i<5;i++)
+		scanf("%d",&a[i]);
+	printf("Enter the other binary number with spaces :");
+	for(i=1;i<5;i++)
+		scanf("%d",&b[i]);
+	printf("-----------------------------------------------------\n");
+	for(i=4;i>=0;i--)
+	{
+		if(a[i]==0&&b[i]==0)
+		{
+			if(k==0)
+			{
+				c[i]=0;
+			}
+			else
+			{
+				c[i]=1;
+			}
+			k=0;
+		}
+		else if((a[i]==0&&b[i]==1)||(a[i]==1&&b[i]==0))
+		{
+			if(k==0)
+			{
+				c[i]=1;
+			}
+			else
+			{
+				c[i]=0;
+				k=1;
+			}
+		}
+		else if(a[i]==1&&b[i]==1)
+		{
+			if(k==0)
+			{
+				c[i]=0;
+				k=1;
+			}
+			else
+			{
+				c[i]=1;
+				k=1;
+			}
+		}
+	}
+	printf("                                        ");
+	printf("%d |",c[0]);
+	for(i=1;i<5;i++)
+	printf("%d ",c[i]);
+	return 0;
+	}
